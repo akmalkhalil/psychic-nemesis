d0901754c12b1216949564584c37b899e0dfddc8
Akmal Khalil <silenttechy@users.noreply.github.com>
2014-12-12 18:06:57 +0000

    Update card class.py

--- card class.py
+++ card class.py
@@ -1,5 +1,5 @@
 import pygame
-##from pygame.locals import *
+from pygame.locals import *
 
 class Card():
     def __init__(self, faceVal, suit):
@@ -9,11 +9,11 @@ class Card():
     
 
 
-##pygame.init()
-##fpsClock = pygame.time.Clock()
-##windowSurf = pygame.display.set_mode((640,480))
-##
-##num = 0
+pygame.init()
+fpsClock = pygame.time.Clock()
+windowSurf = pygame.display.set_mode((640,480))
+
+num = 0
 
 deck = []
 suits = ('♠', '❤', '♣', '♦')
@@ -29,45 +29,46 @@ for i in range(4):
         blob = 'diamonds'
     for j in range(1,14):
             if j == 1:
-                    face = 'A'
-                    bob = 'ace'
+                face = 'A'
+                bob = 'ace'
             elif j == 11:
-                    face = 'J'
-                    bob = 'jack'
+                face = 'J'
+                bob = 'jack'
             elif j == 12:
-                    face = 'Q'
-                    bob = 'queen'
+                face = 'Q'
+                bob = 'queen'
             elif j == 13:
-                    face = 'K'
-                    bob = 'king'
+                face = 'K'
+                bob = 'king'
             else:
-                    face = str(j)
-                    bob = face
+                face = str(j)
+                bob = face
             deck.append(Card(face,suits[i]))
             deck[-1].img = pygame.image.load('Playing Cards\\PNG-cards-1.3\\'+bob+'_of_'+blob+'.png')
             deck[-1].img = pygame.transform.scale(deck[-1].img,(int(deck[-1].img.get_width()/5),int(deck[-1].img.get_height()/5)))
 
 
+running = True
+while running:
+    windowSurf.blit(deck[num].img,(100,100))
 
-##while True:
-##    windowSurf.blit(deck[num].img,(100,100))
-##
-##    for event in pygame.event.get():
-##        if event.type == QUIT:
-##            pygame.quit()
-##        elif event.type == KEYDOWN:
-##            if event.key == K_UP:
-##                if num == len(deck)-1:
-##                    num = 0
-##                else:
-##                    num += 1
-##            elif event.key == K_DOWN:
-##                if num == 0:
-##                    num = len(deck)-1
-##                else:
-##                    num -=1
-##            if event.key == K_ESCAPE:
-##                pygame.event.post(pygame.event.Event(QUIT))
-##
-##    pygame.display.update()
-##    fpsClock.tick(30)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+        elif event.type == KEYDOWN:
+            if event.key == K_UP:
+                if num == len(deck)-1:
+                    num = 0
+                else:
+                    num += 1
+            elif event.key == K_DOWN:
+                if num == 0:
+                    num = len(deck)-1
+                else:
+                    num -=1
+            if event.key == K_ESCAPE:
+                pygame.event.post(pygame.event.Event(QUIT))
+                running = False
+
+    pygame.display.update()
+    fpsClock.tick(30)
