2461ba2f02d750f22ac0f24bc1dbdfa200033971
Akmal Khalil <silenttechy@users.noreply.github.com>
2014-12-24 17:25:09 +0000

    Update speed testing hand.py

--- speed testing hand.py
+++ speed testing hand.py
@@ -1,5 +1,3 @@
-#best name i could come up with#
-
 #need a class for players in speed
 
 class Player():
@@ -30,34 +28,15 @@ def deal(cards):
                 array.append(cards[j])
             ps.append(array)
             count = lim
+        ps.append([cards[x] for x in range(count, len(cards))])
         return ps
 
 deck = [x for x in range(52)]
 p1Hand = [deck[x] for x in range(0,int(len(deck)/2))]
 p2Hand = [deck[x] for x in range(int(len(deck)/2), len(deck))]
 
-if len(p1Hand)<15:
-    print("p1 Has won")
-elif len(p2Hand)<15:
-    print("p2 ahs won")
-else:
-    p1p1 = p1Hand[0]
-    p1p2 = [p1Hand[1],p1Hand[2]]
-    p1p3 = [p1Hand[x] for x in range(3,6)]
-    p1p4 = [p1Hand[x] for x in range(6, 10)]
-    p1p5 = [p1Hand[x] for x in range(10, 16)]
-    p1P  = [p1Hand[x] for x in range(16, len(p1Hand))]
-
-
-ps = []
-count =0
-for i in range(5):
-    array = []
-    lim = count +i + 1
-    for j in range(count, lim):
-        array.append(p1Hand[j])
-    ps.append(array)
-    count = lim
+p1ps = deal(p1Hand)
+p2ps = deal(p2Hand)
     
 
 
